7e8a31664218210c866caebe0facb261cc4d89cd
Li_Daoxin <lidaoxinldx@gmail.com>
2025-01-18 00:25:55 +0800

    level-1

--- src/main/java/Yuki.java
+++ src/main/java/Yuki.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 public class Yuki {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________\n" +
                 " Hello! I'm Yuki\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
+                "____________________________________________________________\n");
+
+        String endString = "bye";
+        String currentString = " ";
+        Scanner scanner = new Scanner(System.in);
+        currentString = scanner.nextLine();
+        while (!currentString.equals(endString)) {
+            System.out.println("____________________________________________________________\n" +
+                    " " + currentString + "\n" +
+                    "____________________________________________________________\n");
+            currentString = scanner.nextLine();
+        }
+        System.out.println("____________________________________________________________\n" +
                 " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n");
     }
