d36267f159d14e4766d9b4160ad8e2b06cd71a36
Li_Daoxin <lidaoxinldx@gmail.com>
2025-01-18 00:48:21 +0800

    level-2

---
+++ src/main/java/Task.java
@@ -0,0 +1,14 @@
+public class Task {
+    private final String taskName;
+    private boolean isDone;
+
+    public Task(String taskName) {
+        this.taskName = taskName;
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return taskName;
+    }
+}
--- src/main/java/Yuki.java
+++ src/main/java/Yuki.java
@@ -11,10 +11,24 @@ public class Yuki {
         String currentString = " ";
         Scanner scanner = new Scanner(System.in);
         currentString = scanner.nextLine();
+        Task[] taskList = new Task[105];
+        int taskCount = 0;
+
         while (!currentString.equals(endString)) {
+            if (currentString.equals("list")) {
+                System.out.println("____________________________________________________________\n");
+                for (int i = 1; i <= taskCount; i++) {
+                    System.out.println(i + ". " + taskList[i].toString());
+                }
+                System.out.println("____________________________________________________________\n");
+                currentString = scanner.nextLine();
+                continue;
+            }
+
             System.out.println("____________________________________________________________\n" +
-                    " " + currentString + "\n" +
+                    " added: " + currentString + "\n" +
                     "____________________________________________________________\n");
+            taskList[++taskCount] = new Task(currentString);
             currentString = scanner.nextLine();
         }
         System.out.println("____________________________________________________________\n" +
