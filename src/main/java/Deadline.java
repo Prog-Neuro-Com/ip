7603d32d72127174146a45ed14c147f8c419b874
Li_Daoxin <lidaoxinldx@gmail.com>
2025-01-26 21:03:29 +0800

    branch-level-8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,18 +1,25 @@
-public class Deadline extends Task{
-    private final String deadline;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-    public Deadline(String taskName, String deadline) {
+public class Deadline extends Task{
+    private final LocalDate deadline;
+
+    public Deadline(String taskName, String deadline) throws YukiException {
         super(taskName);
-        this.deadline = deadline;
+        try {
+            this.deadline = LocalDate.parse(deadline);
+        } catch (Exception e) {
+            throw new YukiException("Invalid date format. Please use yyyy-mm-dd");
+        }
     }
 
     @Override
     public String toFileString() {
-        return "D | " + super.toFileString() + " | " + deadline;
+        return "D | " + super.toFileString() + " | " + deadline.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + deadline + ")";
+        return "[D]" + super.toString() + " (by: " + deadline.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
