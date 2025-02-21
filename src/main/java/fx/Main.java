fe29d36e6f474013b60a23e6ac58221777cc5568
Li_Daoxin <lidaoxinldx@gmail.com>
2025-02-21 20:03:34 +0800

    Add user guide

--- src/main/java/fx/Main.java
+++ src/main/java/fx/Main.java
@@ -27,6 +27,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Yuki");
             fxmlLoader.<MainWindow>getController().setDuke(yuki);  // inject the Duke instance
             stage.show();
         } catch (IOException e) {
